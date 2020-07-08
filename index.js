8e89d012a21509c8edbe7c6f7757a9da916058d2
Showbed <santana.obed@outlook.com>
2020-07-08 18:27:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.get("/test", (request, response) => {
+    response.send("Hello from API)");
+});
+
+
 app.listen(9999, () => {
     console.log("App is listening!");
 })
+
